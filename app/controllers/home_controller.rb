a67ddc6f9f5fb3e704baf7d349c7ff8dc9c5272a
Sean Knox <knoxville@gmail.com>
2012-11-15 15:19:39 -0600

    Changed the Google Calendar resource to Events in the home_controller.rb. Added calendarId to resource call. Added debug output to the home#index view.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -5,10 +5,23 @@ class HomeController < ApplicationController
     if (user_signed_in? )
       client = ClientBuilder.get_client(current_user)
       service = client.discovered_api('calendar', 'v3')
-      result = client.execute(:api_method => service.calendar_list.list)
+      resource = client.execute(:api_method => service.events.list, 
+                                :parameters => {'calendarId' => 'primary'}) 
 
-      @calendars = result.data
+      @calendars = resource.data
+      # @calendars = resource.data.as_json
     end
 
   end
 end
+
+
+
+## some debuggin' code for rails console if you want to test manually
+
+# require 'google/api_client'
+# require 'client_builder'
+# current_user = User.find(1)
+# client = ClientBuilder.get_client(current_user)
+# service = client.discovered_api('calendar', 'v3')
+# resource = client.execute(:api_method => service.events.list, :parameters => {'calendarId' => 'primary'}) 
