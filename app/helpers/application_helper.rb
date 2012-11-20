bed03168279578056a3eef7416c1b111c7e56c44
Sean Knox <knoxville@gmail.com>
2012-11-20 12:54:58 -0600

    Refactored code when checking for location by moving that check into a helper method, then calling that method in the view.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+
+	def location_for(event)
+    if event[:location]
+        "Where: #{event[:location]}"
+    else
+        "Where: <em>No location yet!</em>".html_safe
+    end
+	end
+
 end
