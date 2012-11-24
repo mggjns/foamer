2162c70ed8c32e150a463479461d72805f4428cd
Sean Knox <knoxville@gmail.com>
2012-11-24 15:19:31 -0800

    Removed some old comments/cruft

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,3 @@
-# TODO: Can we cache the API query to Google somehow, so to speed things up?
 class HomeController < ApplicationController
   require 'google/api_client'
   require 'client_builder'
@@ -8,6 +7,7 @@ class HomeController < ApplicationController
     if (user_signed_in? )
       # Only query Google Calendar API if we don't have any events in the DB
       if current_user.events.where("start >= ?", Date.today).size == 0
+        # Find me in home_helper.rb
         google_query
       end
       # Fetch today's events from the DB to display in view
