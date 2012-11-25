9825c3928edd16457cc994a842e7e1ff44f988c2
Sean Knox <knoxville@gmail.com>
2012-11-24 22:40:18 -0600

    Whoops, helpers are only for views. Put the Google API query code back into HomeController, but as its own method

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,17 +1,17 @@
 module ApplicationHelper
 
 	def location_for(event)
-    if event[:location]
-        "Where: #{event[:location]}"
-    else
-        "Where: <em>No location yet!</em>".html_safe
-    end
+        if event[:location]
+            "Where: #{event[:location]}"
+        else
+            "Where: <em>No location yet!</em>".html_safe
+        end
 	end
 
 	def description_for(event)
-    if event[:description]
-        "#{event[:description]}"
-    end
+        if event[:description]
+            "#{event[:description]}"
+        end
 	end
 
 end
