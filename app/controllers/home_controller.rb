ac41f958a27c1a8cb40267689e6af3a9d4ea2154
Sean Knox <knoxville@gmail.com>
2012-11-26 19:57:17 -0600

    Check if lat_lng cookies are set before trying to assign an instance variable to it

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -5,7 +5,10 @@ class HomeController < ApplicationController
   def index
 
     if (user_signed_in? )
-      @lat_lng = cookies[:lat_lng].split("|")
+      # TODO: Is this the best way to check if we have the lat_lng cookie set?
+      if (cookies[:lat_lng])
+        @lat_lng = cookies[:lat_lng].split("|")
+      end
       # Only query Google Calendar API if we don't have any events in the DB
       if current_user.events.where("start >= ?", Date.today).size == 0
         # Find me in application_controller.rb (as a helper method)
